ad8a46d6770d3e11b7a3f6f4bb660bebc039ff27
SophieTax <s.tascedda@student.maastrichtuniversity.nl>
2022-07-12 12:05:54 +0200

    Updated sample_covariance function and notebook

--- sample_covariance.R
+++ sample_covariance.R
@@ -1,16 +1,10 @@
-sample_covariance = function(data, factor_name, level_name) {
-  data_sub = data[which(data[factor_name] == level_name),] %>% droplevels
-  region_ids = str_detect(names(ts_subject_info),"R") %>% which
-  subject_ids = table(data_sub$Subject) %>% names
-  s_cov = list()
-  K = length(subject_ids)
-  P = length(region_ids)
-  s_cov$YY = array(dim = c(K,P,P))
-  for(i in 1:K) {
-    Y = subset(data_sub,Subject == subject_ids[i])[,region_ids]
-    Y_c = scale(Y,center = TRUE,scale = FALSE)
-    s_cov$YY[i,,] =( t(Y_c) %*% Y_c)/K
+sample_covariance=function(data,regions,subj){
+  K=length(data)
+  P=length(regions)
+  s_cov=list()
+  s_cov$cov=array(dim=c(K,P,P))
+  for(i in 1:K){
+    s_cov$cov[i,,]=cov(as.matrix(Y_c_short[[i]]))
   }
-  s_cov$N = table(data_sub$Subject)
   s_cov
 }
