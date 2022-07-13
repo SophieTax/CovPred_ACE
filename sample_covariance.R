4969086e3d725ef17486d43e8128221a65c73f44
SophieTax <s.tascedda@student.maastrichtuniversity.nl>
2022-07-13 10:21:45 +0200

    Implemented sample_covariance.R for both short and conventional sleepers

--- sample_covariance.R
+++ sample_covariance.R
@@ -4,7 +4,7 @@ sample_covariance=function(data,regions,subj){
   s_cov=list()
   s_cov$cov=array(dim=c(K,P,P))
   for(i in 1:K){
-    s_cov$cov[i,,]=cov(as.matrix(Y_c_short[[i]]))
+    s_cov$cov[i,,]=cov(as.matrix(data[[i]]))
   }
   s_cov
 }
