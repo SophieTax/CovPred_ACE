26968f8a7349f85ee0ccffa60bdadc964bb405ae
SophieTax <s.tascedda@student.maastrichtuniversity.nl>
2022-08-18 12:03:21 +0200

    Modified some functions, ttest process correct, plot

--- sample_covariance.R
+++ sample_covariance.R
@@ -2,9 +2,8 @@ sample_covariance=function(data,regions,subj){
   K=length(data)
   P=length(regions)
   s_cov=list()
-  s_cov$cov=array(dim=c(P,P,K), dimnames = list(regions,regions,subj))
   for(i in 1:K){
-    s_cov$cov[,,i]=cov(as.matrix(data[[i]]))
+    s_cov[[i]]=cov(as.matrix(data[[i]]))
   }
   
   s_cov
