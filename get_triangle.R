9cba34f88ae2b7fccd8d2817e83a71d4df4737c0
SophieTax <s.tascedda@student.maastrichtuniversity.nl>
2022-08-26 14:38:28 +0200

    Added option for upper and lowe triangle

--- get_triangle.R
+++ get_triangle.R
@@ -1,6 +1,14 @@
-get_triangle=function(data){
+get_triangle=function(data, triangle){
   m<-data
+  if (triangle=="UPPER"){
+  #gets upper triangle
+  t<-lower.tri(m,diag=TRUE)
+  m[t]=NA
+  }
+  else {
+  #gets lower triangle
   t<-upper.tri(m, diag = TRUE)
   m[t]=NA
+  }
   m
 }
