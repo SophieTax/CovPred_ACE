91f4bd9f7b0d77df96e1fa43b71f3276a0750cc8
SophieTax <s.tascedda@student.maastrichtuniversity.nl>
2022-10-24 16:50:53 +0200

    removed unnecessary files

--- cov_pred_subj.R
+++
@@ -1,8 +0,0 @@
-cov_pred_subj = function(gamma,beta,X,gginv){
-  a<-gginv%*%gamma
-  #b<-exp(beta[1]+X[1,2]*beta[2])
-  b<-exp(beta[1]+X*beta[2])
-  c<-gamma%*%gginv
-  sig_hat_si<-a%*%b%*%c
-  as.matrix(sig_hat_si)
-}
--- gamma_inv.R
+++
@@ -1,5 +0,0 @@
-gamma_inv = function(gamma_vec){
-  gg=gamma_vec%*%t(gamma_vec)
-  inv=ginv(gg)
-  inv
-}
